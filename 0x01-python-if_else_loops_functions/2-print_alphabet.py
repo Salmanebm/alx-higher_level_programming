8bf3d11d80e23a45db0f7455f89687c6be81b602
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-05 08:08:45 -0700

    next alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 start = ord('a')
 end = ord('z') + 1
-alphabet = ""
 for letter_dec in range(start, end):
     letter = chr(letter_dec)
-    alphabet += letter
-print(alphabet)
+    print(letter)
