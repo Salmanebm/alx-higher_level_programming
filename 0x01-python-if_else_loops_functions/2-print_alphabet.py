e7858de3995f9782c41264696dfb3adb0a2a4cb6
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-05 08:34:27 -0700

    next alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-start = ord('a')
-end = ord('z') + 1
-alphabet = ''.join(chr(letter_code) for letter_code in range(start, end))
-print(alphabet)
+for letter in range(97, 123):
+        print("{}".format(chr(letter)), end="")
+
