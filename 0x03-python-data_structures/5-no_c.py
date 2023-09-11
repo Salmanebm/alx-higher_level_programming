ea00bcbe01ae974d3def29f6e3515045ad025f37
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-11 11:14:09 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    alt = ""
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            alt += " "
-        else:
-            alt += c
-    return (alt)
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
