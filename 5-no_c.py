ef0eb4a724f0edefc38b9e245e4f49c96a15def4
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-13 17:40:42 +0100

    remove not necessary

--- 5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    alt = ""
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            alt += " "
-        else:
-            alt += c
-    return (alt)
