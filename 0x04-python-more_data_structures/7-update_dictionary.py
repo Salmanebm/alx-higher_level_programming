033354fc5abd96c0aeea5ff962f73e55a3907a18
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-14 16:38:14 +0100

    new

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,6 @@
 def update_dictionary(a_dictionary, key, value):
     if not a_dictionary:
         return
-    a_dictionary['key'] = value
+    if key not in a_dictionary:
+        a_dictionary['key'] = value
     return (a_dictionary)
