7f028c74b8387f6a20df2267c9639f8e02eb7076
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-17 14:52:43 +0100

    AA

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    char = chr(i)
-    if char.islower():
-        print(char, end='')
-    else:
-        print(char.lower(), end='')
-print()
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
