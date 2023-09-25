4a4aa9b15b9a275a747c70adfadb21909635a1f0
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-25 21:45:32 +0100

    Cont

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{}".format(my_list[0]), end="")
             red += 1
         except (IndexError, TypeError):
-            break
+            continue
     print("")
     return (red)
