6f0210e64c923a601fe17f7078891a8adbb2d878
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-26 22:29:39 +0100

    TOC

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """show a square."""
 
-    def __init__(self, size):
+    def __init__(self, size = 0):
         """Initialize a new Square.
 
         Args:
@@ -16,5 +16,4 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
         
