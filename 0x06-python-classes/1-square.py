7298dfc9008e944a097fba5ae8612502b6a33deb
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-26 22:21:31 +0100

    T1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """show a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: private instance
+        """
+        self.__size = size
