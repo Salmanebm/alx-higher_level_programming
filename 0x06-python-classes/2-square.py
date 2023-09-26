436588b03b53bbe91610c19f7276c88dd6fab368
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-26 22:28:06 +0100

    T2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """show a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: private instance
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        
