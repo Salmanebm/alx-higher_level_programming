541f28f9be54521938d9177cb6bbf41287ffec21
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-12-11 06:19:15 -0800

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let lang = ['C is fun', 'Python is cool', 'JavaScript is amazing');
-for(i = 0; i < 3; i++)
-{	console.log(lang[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
