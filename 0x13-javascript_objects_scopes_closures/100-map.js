dd17b745cc000a6f55476238dc4bb869631d6c8e
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-12-13 07:17:26 -0800

    rer

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const { list } = require('./100-data');
-
-const newList = list.map((value, index) => value * index);
-
-console.log('Initial List:', list);
-console.log('New List:', newList);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
