45a5887cd0edb4e5b54a71c457ad3ab46b4f8943
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-12-13 03:29:44 -0800

    draw

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (this.isValidDimension(w) && this.isValidDimension(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  isValidDimension (dimension) {
+    return typeof dimension === 'number' && dimension > 0 && Number.isInteger(dimension);
+  }
+  
+  print () {
+    if (this.width === 0 || this.height === 0) {
+      console.log('Empty Rectangle');
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
