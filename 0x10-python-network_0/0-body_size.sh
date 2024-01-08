1935b4d8b5770d874f3db9ecd43f40285f5d0549
Salmane Ben Messaoud <salmanesbm@gmail.com>
2024-01-08 12:03:58 -0800

    3l

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-
-
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
+# Use curl to send a request and store the response in a variable
 
 url=$1
-
-response=$(curl -sI "$url")
-
-content_length=$(echo "$response" | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
-
-if [ -n "$content_length" ]; then
-    echo "Size of the body of the response: ${content_length} bytes"
-else
-    echo "Content-Length not found in the response headers"
-fi
+curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r'
